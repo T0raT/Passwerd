9a1490c0b64569a9e42b15c8d78548d4b15a992d
T0raT <tigerliu@bu.edu>
2025-11-08 17:30:35 -0500

    feat: adds basic main.ts for basic functionality. Can generate and output password now.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
+import PassGen, { type Config } from "./lib/entropy";
 
+const fakeConfig = {
+  upper: true,
+  lower: true,
+  nums: true,
+  symb: true,
+  length: 16,
+};
+
+const genPassBtn = document.getElementById("gen-btn");
+const outputField = document.querySelector<HTMLElement>(".output");
+
+const renderToField = (config: Config) => {
+  if (!outputField) return;
+  outputField.textContent = PassGen(config);
+};
+
+genPassBtn?.addEventListener("click", (event) => {
+  event.preventDefault();
+  renderToField(fakeConfig);
+});
